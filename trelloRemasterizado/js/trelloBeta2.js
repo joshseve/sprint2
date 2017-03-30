0fa74fe6a09b48cd511fd4b8b2951b8fcd585640
Paola Jocelin Septimo Velazquez <Paola Jocelin Septimo Velazquez>
2017-03-30 10:39:39 -0600

    Código refactorizado (Trello Remasterizado)

--- trelloRemasterizado/js/trelloBeta2.js
+++ trelloRemasterizado/js/trelloBeta2.js
@@ -1,43 +1,28 @@
-console.log("hola");
+    console.log("hola");
 
-function add_li()
-{
-    var tareaNueva=document.getElementById("tareaNueva").value;
-    if(tareaNueva.length>0)
+    function add_li()
     {
-        if(find_li(tareaNueva))
-        {
-            var li=document.createElement('li');
-            li.id=tareaNueva;
-            li.innerHTML=tareaNueva + " " + "<span onclick='eliminar(this);'> ✖ </span>";
-            document.getElementById("listaDesordenada").appendChild(li);
-        }
+        var tareaNueva=document.getElementById("tareaNueva").value;
+        var li=document.createElement('li');
+        li.id=tareaNueva;
+        li.innerHTML=tareaNueva + " " + "<span onclick='eliminar(this);'> ✖ </span>";
+        document.getElementById("listaDesordenada").appendChild(li);
+        
+        return false;
     }
-    return false;
-}
 
-function find_li(contenido)
-{
-    var el = document.getElementById("listaDesordenada").getElementsByTagName("li");
-    for (var i=0; i<el.length; i++)
+
+    function eliminar(elemento)
     {
-        if(el[i].innerHTML==contenido)
-            return false;
+        var id=elemento.parentNode.getAttribute("id");
+        node=document.getElementById(id);
+        node.parentNode.removeChild(node);
+        // return false;
     }
-    return true;
-}
 
-function eliminar(elemento)
-{
-    var id=elemento.parentNode.getAttribute("id");
-    node=document.getElementById(id);
-    node.parentNode.removeChild(node);
-    // return false;
-}
-
-function borraTodo(listaDesordenada)
-{
-    var id=elemento.parentNode.getAttribute("id");
-    node=document.getElementById(id);
-    node.parentNode.removeChild(node);
-}
+    function borraTodo(listaDesordenada)
+    {
+        var id=elemento.parentNode.getAttribute("id");
+        node=document.getElementById(id);
+        node.parentNode.removeChild(node);
+    }
